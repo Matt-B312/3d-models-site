999b6b2c5335e0fc0564b19b458ef88dfb23d1e3
[Dylan Kotzer] <[dKotzer@gmail.com]>
2022-04-01 16:41:06 -0400

    Model work, and work on image viewing/upload functionality

--- main_app/views.py
+++ main_app/views.py
@@ -1,18 +1,16 @@
-
-
-# Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
-
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-
 from django.views.generic import ListView, DetailView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+# from .forms import UploadFileForm
+# from .forms import ModelFormWithFileField
 import os
 
+# Create your views here.
 
 def home(request):
     print(os.getenv('NAME'))
@@ -33,3 +31,14 @@ def signup(request):
     form = UserCreationForm()
     context = {'form':form, 'error_message': error_message}
     return render(request, 'registration/signup.html', context)
+
+# def upload_file(request):
+#     if request.method == 'POST':
+#         form = ModelFormWithFileField(request.POST, request.FILES)
+#         if form.is_valid():
+#             # file is saved
+#             form.save()
+#             return HttpResponseRedirect('/success/url/')
+#     else:
+#         form = ModelFormWithFileField()
+#     return render(request, 'upload.html', {'form': form})
