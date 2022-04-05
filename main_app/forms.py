f0bb5cfacb0119bf0a3ca849f203d23eddcc97a3
[Dylan Kotzer] <[dKotzer@gmail.com]>
2022-04-05 14:15:13 -0400

    broken model import fixed

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,7 +1,8 @@
 
-from django.contrib.auth.models import User, Account
+from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserChangeForm
 from django.forms import ModelForm
+from .models import Account
 
 
 class AccountCreate(ModelForm):
