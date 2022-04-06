16be59879d7ff476a1d4b3ad891f63b1cf0afa0c
[Dylan Kotzer] <[dKotzer@gmail.com]>
2022-04-06 15:59:23 -0400

    arghssadasdasd

--- models_site/wsgi.py
+++ models_site/wsgi.py
@@ -13,4 +13,5 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'models_site.settings')
 
-application = get_wsgi_application()heroku logs --tail
+application = get_wsgi_application()
+
