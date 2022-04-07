7d5507142b1b7f75a3ae1d6d2390cfbc94e68e83
[Dylan Kotzer] <[dKotzer@gmail.com]>
2022-04-06 21:43:28 -0400

    text test

--- main_app/models.py
+++ main_app/models.py
@@ -45,7 +45,7 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     model = models.FileField(blank=True, null=True, upload_to="models/%Y/%m/$D/",default="/media/models/Earth.glb")
     # images = models.CharField(max_length=2000, default=None, blank=True, null=True )
-    text_content = models.TextField(max_length=1000, default=None, blank=True, null=True)
+    text_content = models.TextField(max_length=500, default=None, blank=True, null=True)
     tags = models.TextField(max_length=1000, default=None, blank=True, null=True)
     downloads = models.IntegerField(default=0)
     type = models.CharField(max_length=50, default="GLB")
